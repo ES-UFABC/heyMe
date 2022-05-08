b962bc7c4dc767544e5487c36c07583b3616eb7d
Thiago-SMerces <thiago_merces@outlook.com>
2022-05-07 21:01:06 -0300

    Get patients list on therapyst screen

--- hey_me/src/Welcome/index.js
+++ hey_me/src/Welcome/index.js
@@ -2,6 +2,27 @@ import React, { useState, useEffect } from "react";
 import Header from '../components/Header';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import './welcome.css';
+
+var table_data = [];
+
+function RenderTable () {
+	// console.log("render table", table_data);
+	return <> {
+		table_data.map(
+			(item)=>{
+				return(
+					<tr key={item['id']}>
+						<td>{item['name']}</td>
+						<td>{item['email']}</td>
+						<td><i className="fa-solid fa-comment-dots fa-1.5x" style={{color:"rgb(220, 206, 206)"}}></i></td>
+					</tr>
+				)
+			}
+		)
+	}
+	</>
+}
 
 function Welcome() {
 	var validate = 2;
@@ -30,10 +51,11 @@ function Welcome() {
 						.then(function(response) {
 							res = response.data;
 							console.log(res['success']);
-
 							if (res['success'] == true) {
 								console.log('patients = ', res['patients']);
+								table_data = res['patients'];
 								isTherapist = 1;
+								setterapistload(1);
 							}
 				//Perform action based on response
 					})
@@ -42,8 +64,6 @@ function Welcome() {
 				//Perform action based on error
 					});
 		}
-		setterapistload(1);
-
 	}
 	}, []);
 	
@@ -55,10 +75,52 @@ function Welcome() {
 	const [terapistload, setterapistload] = useState(2);
 
 		return (validate) ? (
-			<div className="welcome">
-				<Header />
-				<h1>Bem-Vindo ao heyMe!</h1>
-				{callList}
+			<div>
+				<div className="welcome">
+					<Header />
+					<h1>Bem-Vindo ao heyMe!</h1>
+					{callList}
+				</div>
+				<br></br>
+					{terapistload == 1 &&
+					<div className="tbl-all">
+						<div className="tbl-header">
+							<table cellpadding="0" cellspacing="0" border="0">
+								<thead>
+									<tr>
+										<th>
+											Pacientes
+										</th>
+										<th>
+											Email
+										</th>
+										<th>
+											Chat
+										</th>
+									</tr>
+								</thead>
+							</table>
+						</div>
+  						<div className="tbl-content">
+							<table cellpadding="0" cellspacing="0" border="0">
+								<tbody>
+								{/* <tr>
+										<td>
+											Pacientes
+										</td>
+										<td>
+											Email
+										</td>
+										<td>
+											Chat
+										</td>
+									</tr> */}
+									<RenderTable/>
+								</tbody>
+							</table>
+						</div>
+					</div>				
+					}
 			</div>
 		) : (
 			<div>
