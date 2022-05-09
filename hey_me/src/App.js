dd83065ae09c26c9a52aec30515b9b7e1944c330
Thiago-SMerces <thiago_merces@outlook.com>
2022-05-09 20:50:54 -0300

    bugfixes

--- hey_me/src/App.js
+++ hey_me/src/App.js
@@ -7,29 +7,11 @@ import Welcome from './Welcome';
 import Diario from './Diario';
 import Chat from './Chat';
 import Messages from './Messages';
-import { CometChat } from "@cometchat-pro/chat";
-
 	
 function App() {
-	const appID = "209080691110ce73";
-	const region = "us";
-	const appSetting = new CometChat.AppSettingsBuilder()
-	  .subscribePresenceForAllUsers()
-	  .setRegion(region)
-	  .build();
-	CometChat.init(appID, appSetting).then(
-	  () => {
-		console.log("Initialization completed successfully");
-		// You can now call login function.
-	  },
-	  (error) => {
-		console.log("Initialization failed with error:", error);
-		// Check the reason for error and take appropriate action.
-	  }
-	);
 	// amazon: https://cors-everywhere.herokuapp.com/http://heymeapi-env-1.eba-5rahmizp.us-east-1.elasticbeanstalk.com
-	localStorage.setItem("api-endpoint","http://localhost:5000");
-	localStorage.setItem("api-origin", "http://localhost:3000");
+	localStorage.setItem("api-endpoint","https://cors-everywhere.herokuapp.com/http://heymeapi-env-1.eba-5rahmizp.us-east-1.elasticbeanstalk.com");
+	localStorage.setItem("api-origin", "https://main.d1w1cxbdfenujy.amplifyapp.com/");
 	// amazon: "https://main.d1w1cxbdfenujy.amplifyapp.com/"
 	// local: http://localhost:3000
 	return (
