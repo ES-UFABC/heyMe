98c6cce4d01a241eabe776af00b9668f601832c5
lucasfks <60987105+lucasfks@users.noreply.github.com>
2022-05-12 20:57:16 -0300

    Register redirects to Welcome

--- hey_me/src/Register/index.js
+++ hey_me/src/Register/index.js
@@ -3,6 +3,54 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { CometChat } from "@cometchat-pro/chat";
 
+function handleClickLogin() {
+	let res = {};
+	let sendData = {
+		'email': document.getElementById('email').value,
+		'password': document.getElementById('password').value
+	};
+	let config = {
+		header:{
+			"Origin": localStorage.getItem("api-origin")
+		}
+	};
+
+	var ustr = (document.getElementById('email').value);
+	var UID =  ustr.substring(0, ustr.indexOf('@'));
+	var authKey = "8f1ed710ae8fbed4da3f774ed784e1458c18f4a1";
+
+
+	CometChat.login(UID, authKey).then(
+	  (User) => {
+		console.log("Login Successful:", { User });
+		localStorage.setItem("userid", User.uid);
+		localStorage.setItem("useremail", UID);
+	  },
+	  (error) => {
+		console.log("Login failed with exception:", { error });
+		// User login failed, check error and take appropriate action.
+	  }
+	);
+
+	axios.post(`${localStorage.getItem("api-endpoint")}/login_back`, sendData, config)
+		.then(function(response){
+			res = response.data;
+			console.log(res['success']);
+
+			if (res['success'] == true) {
+				localStorage.setItem('token', res['access_token']);
+				localStorage.setItem('isTherapist', res['isTherapist']);
+				console.log('login type', res['isTherapist']);
+			}
+   //Perform action based on response
+	})
+	.catch(function(error){
+		console.log(error);
+   //Perform action based on error
+	});
+	
+}
+
 function Switch({ isOn, handleToggle }) {
 	return (
 	  <>
@@ -68,6 +116,7 @@ function Register() {
 				res = response.data;
 				console.log(res['success']);
 				if (res['success'] == true) {
+					handleClickLogin();
 					navigate('/welcome');
 				}
 		})
@@ -76,6 +125,7 @@ function Register() {
 			window.confirm('Dados incorretos! Por favor, verifique novamente');
 		    console.log(error);
 		});
+
 		
 	}
 	const [value, setValue] = useState(false);
